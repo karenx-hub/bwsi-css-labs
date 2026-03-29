20ecb606b53971bcd96508424226b889dcf80169
karenx-hub <karen.xiao@warriorlife.net>
2026-03-28 20:40:46 -0700

    fix online change

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -8,6 +8,7 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 def main():
     print("Hello World!")
 
+    name = "KThex with onine changes" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
 
